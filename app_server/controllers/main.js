36e287a41c0b97d6b83406838e68b9ea8115fe22
Emmanuella Nyamekye <emmanuella.g.nyamekye@students.ittralee.ie>
2024-10-26 16:09:56 +0100

    Json array of objects done

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,8 +1,56 @@
+/* GET 'home' page */
+const productList = function (req, res) {
+    const procuduct = [
+      {
+        name: 'Pair of Chairs',
+        imageUrl: 'https://github.com/EmmaNyamekye/FurnishUp/blob/main/public/images/chairs_image.png?raw=true',
+        description: 'Warm and inviting acacia wood chairs, perfect for adding a touch of nature to your outdoor space.',
+        location: 'Tralee',
+        contact: 'fname.lname@example.com'
+      },
+      {
+        name: 'Cutting Board and Cutlery Set',
+        imageUrl: 'https://github.com/EmmaNyamekye/FurnishUp/blob/main/public/images/cutting_board_image.png?raw=true',
+        description: 'This wooden cutting board and utensil set shows signs of use, but is still in excellent working order.',
+        location: 'Killarney',
+        contact: 'fname.lname@example.com'
+      },
+      {
+        name: 'Pot with Lid',
+        imageUrl: 'https://github.com/EmmaNyamekye/FurnishUp/blob/main/public/images/pot_image.png?raw=true',
+        description: 'This versatile stainless steel pot is ideal for preparing soups, sauces, and more. Includes a glass lid for easy monitoring of your cooking.',
+        location: 'Tralee',
+        contact: 'fname.lname@example.com'
+      },
+      {
+        name: 'Dutch Ovens',
+        imageUrl: 'https://github.com/EmmaNyamekye/FurnishUp/blob/main/public/images/dutch_oven_image.png?raw=true',
+        description: 'This red Le Creuset Dutch oven has been well-cared for and shows very little wear.',
+        location: 'Tralee',
+        contact: 'fname.lname@example.com'
+      },
+      {
+        name: 'Table',
+        imageUrl: 'https://github.com/EmmaNyamekye/FurnishUp/blob/main/public/images/table_image.png?raw=true',
+        description: 'This sleek black dining table is perfect for everyday meals and special occasions. It extends to comfortably seat 6-8 people, making it ideal for entertaining guests.',
+        location: 'Killarney',
+        contact: 'fname.lname@example.com'
+      },
+      {
+        name: 'Bed with Matching Bed Frame',
+        imageUrl: 'https://github.com/EmmaNyamekye/FurnishUp/blob/main/public/images/bed_image.png?raw=true',
+        description: 'This single bed has been well-cared for and shows very little wear. Perfect for those seeking quality used furniture.',
+        location: 'Killarney',
+        contact: 'fname.lname@example.com'
+      }
+    ]}
+
 /* GET home page */
 const index = function(req, res){
     res.render('index', { title: 'FurnishUp' });
     };
 module.exports = {
+    productList,
     index
 };
 
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const ctrlMain = require('../controllers/main');
-const ctrlProfiles = require('../controllers/profile'); 
 const ctrlSignUp = require('../controllers/signUp'); 
 const ctrlSignIn = require('../controllers/signIn'); 
 
 /* GET home page. */
 router.get('/', ctrlMain.index);
-router.get('/profile', ctrlProfiles.profile)
 router.get('/signup', ctrlSignUp.signUp)
 router.get('/signIn', ctrlSignIn.signIn)
 module.exports = router;
