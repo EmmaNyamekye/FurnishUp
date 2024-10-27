f17a9eb725ded027ddefbce64a86e91d7a659f2d
Emmanuella Nyamekye <emmanuella.g.nyamekye@students.ittralee.ie>
2024-10-27 02:59:27 +0000

    Navigation has been fixed

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,48 +1,45 @@
-// controllers/main.js
-
-// Shared product data
 const products = [
     {
         name: 'Pair of Chairs',
         imageUrl: 'https://github.com/EmmaNyamekye/FurnishUp/blob/main/public/images/chairs_image.png?raw=true',
         description: 'Warm and inviting acacia wood chairs, perfect for adding a touch of nature to your outdoor space.',
         location: 'Tralee',
-        contact: 'fname.lname@example.com'
+        contact: 'maria.rossella@example.com'
     },
     {
         name: 'Cutting Board and Cutlery Set',
         imageUrl: 'https://github.com/EmmaNyamekye/FurnishUp/blob/main/public/images/cutting_board_image.png?raw=true',
         description: 'This wooden cutting board and utensil set shows signs of use, but is still in excellent working order.',
         location: 'Killarney',
-        contact: 'fname.lname@example.com'
+        contact: 'marco.mengoni@example.com'
     },
     {
         name: 'Pot with Lid',
         imageUrl: 'https://github.com/EmmaNyamekye/FurnishUp/blob/main/public/images/pot_image.png?raw=true',
         description: 'This versatile stainless steel pot is ideal for preparing soups, sauces, and more. Includes a glass lid for easy monitoring of your cooking.',
         location: 'Tralee',
-        contact: 'fname.lname@example.com'
+        contact: 'mario.mario@example.com'
     },
     {
         name: 'Dutch Ovens',
         imageUrl: 'https://github.com/EmmaNyamekye/FurnishUp/blob/main/public/images/dutch_oven_image.png?raw=true',
         description: 'This red Le Creuset Dutch oven has been well-cared for and shows very little wear.',
         location: 'Tralee',
-        contact: 'fname.lname@example.com'
+        contact: 'luigi.bros@example.com'
     },
     {
         name: 'Table',
         imageUrl: 'https://github.com/EmmaNyamekye/FurnishUp/blob/main/public/images/table_image.png?raw=true',
         description: 'This sleek black dining table is perfect for everyday meals and special occasions. It extends to comfortably seat 6-8 people, making it ideal for entertaining guests.',
         location: 'Killarney',
-        contact: 'fname.lname@example.com'
+        contact: 'leo.vinci@example.com'
     },
     {
         name: 'Bed with Matching Bed Frame',
         imageUrl: 'https://github.com/EmmaNyamekye/FurnishUp/blob/main/public/images/bed_image.png?raw=true',
         description: 'This single bed has been well-cared for and shows very little wear. Perfect for those seeking quality used furniture.',
         location: 'Killarney',
-        contact: 'fname.lname@example.com'
+        contact: 'bugs.bunny@example.com'
     }
 ];
 
