89d5fe9ef6d048b14a0804cbac0e8278111a8f03
Emmanuella Nyamekye <Emmanuella.g.Nyamekye@students.ittralee.ie>
2024-12-09 11:28:06 +0000

    Removed SSL files from repo and updated .gitignore

--- app.js
+++ app.js
@@ -1,60 +1,79 @@
 // filename = app.js
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// Core Modules and Dependencies
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
+// Database and Routes
 require('./app_api/models/db');
-
-var indexRouter = require('./app_server/routes/index');
+const indexRouter = require('./app_server/routes/index');
 const apiRouter = require('./app_api/routes/index');
 
-var app = express();
+// Initialize Express App
+const app = express();
 
-// view engine setup
+// View Engine Setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
 
+// Middleware Setup
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Define Routes
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+// Catch 404 Errors
+app.use((req, res, next) => {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+// Error Handler
+app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
-var fs = require('fs');
-var http = require('http');
-var https = require('https');
-var privateKey  = fs.readFileSync('./sslcert/key.pem', 'utf8');
-var certificate = fs.readFileSync('./sslcert/cert.pem', 'utf8');
-var credentials = {key: privateKey, cert: certificate};
+// SSL Certificate Setup
+const https = require('https');
 
-var httpServer = http.createServer(app);
-var httpsServer = https.createServer(credentials, app);
-httpServer.listen(8000);
-httpsServer.listen(443);
+// Check for Environment Variables or Local Files
+let privateKey, certificate;
+
+if (process.env.SSL_KEY && process.env.SSL_CERT) {
+  // Use Environment Variables (Production)
+  privateKey = Buffer.from(process.env.SSL_KEY, 'base64').toString('utf8');
+  certificate = Buffer.from(process.env.SSL_CERT, 'base64').toString('utf8');
+} else {
+  // Use Local Files (Development)
+  const fs = require('fs');
+  privateKey = fs.readFileSync('./sslcert/key.pem', 'utf8');
+  certificate = fs.readFileSync('./sslcert/cert.pem', 'utf8');
+}
+
+// Set SSL Credentials
+const credentials = { key: privateKey, cert: certificate };
+
+// Create Servers
+const http = require('http');
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+// Start Servers
+httpServer.listen(8000, () => console.log("HTTP Server running on port 8000"));
+httpsServer.listen(443, () => console.log("HTTPS Server running on port 443"));
 
 module.exports = app;
+
 
 /*
 app.use('/api', function(req, res, next) { 
